21768786e349beb1389bf0e3b4ba95074f1232d0
mudit67 <mr.mudit.bhardwaj@gmail.com>
2023-11-06 23:28:28 +0530

    feat: Conditionally responding to user input

--- main.cpp
+++ main.cpp
@@ -1,24 +1,51 @@
 #include <iostream>
 
 int main() {
-  // 1 Print Help
-  std::cout << "1: Print Help " << std::endl;
-  // 2 Print exchange stats
-  std::cout << "2: Print exchange stats " << std::endl;
-  // 3 Make an offer
-  std::cout << "3: Make an offer " << std::endl;
-  // 4 Make a bid
-  std::cout << "4: Make a bid " << std::endl;
-  // 5 Print Wallet
-  std::cout << "5: Print Wallet" << std::endl;
-  // 6 Continue
-  std::cout << "6 Continue" << std::endl;
-  std::cout << "-----------------------" << std::endl;
-  std::cout << "Enter your choice: ";
+  while (true) {
 
-  int choice;
+    // 1 Print Help
+    std::cout << "1: Print Help " << std::endl;
+    // 2 Print exchange stats
+    std::cout << "2: Print exchange stats " << std::endl;
+    // 3 Make an offer
+    std::cout << "3: Make an offer " << std::endl;
+    // 4 Make a bid
+    std::cout << "4: Make a bid " << std::endl;
+    // 5 Print Wallet
+    std::cout << "5: Print Wallet" << std::endl;
+    // 6 Continue
+    std::cout << "6 Continue" << std::endl;
+    std::cout << "-----------------------" << std::endl;
+    std::cout << "Enter your choice: ";
 
-  std::cin >> choice;
+    int choice;
 
-  std::cout << "You Chose: " << choice << std::endl;
+    std::cin >> choice;
+
+    std::cout << "You Chose: " << choice << std::endl;
+
+    if (choice == 0) {
+      std::cout << "Invalid Choice. Choose 1-6" << std::endl;
+    }
+    if (choice == 1) {
+      std::cout << "Help - Your Aim is to make money. Analyze the market and "
+                   "make bids and offers."
+                << std::endl;
+    }
+    if (choice == 2) {
+      std::cout << "Market Looks good." << std::endl;
+    }
+    if (choice == 3) {
+      std::cout << "Make an Offer - Enter the amount: " << std::endl;
+    }
+    if (choice == 4) {
+      std::cout << "Make a bid - Enter the amount: " << std::endl;
+    }
+    if (choice == 5) {
+      std::cout << "Your Wallet is emplty!" << std::endl;
+    }
+    if (choice == 6) {
+      std::cout << "Going to next time timeframe" << std::endl;
+    }
+  }
 }
