c1f7d2e164a2690800c17fc66f11745dc6187c09
mudit67 <mr.mudit.bhardwaj@gmail.com>
2023-11-06 22:58:06 +0530

    feat: Basic Structure and menu of the program

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+  // 1 Print Help
+  std::cout << "1: Print Help " << std::endl;
+  // 2 Print exchange stats
+  std::cout << "2: Print exchange stats " << std::endl;
+  // 3 Make an offer
+  std::cout << "3: Make an offer " << std::endl;
+  // 4 Make a bid
+  std::cout << "4: Make a bid " << std::endl;
+  // 5 Print Wallet
+  std::cout << "5: Print Wallet" << std::endl;
+  // 6 Continue
+  std::cout << "6 Continue" << std::endl;
+}
